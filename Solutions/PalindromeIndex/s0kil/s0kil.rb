c743f1d2aaaa69164a712bf7d1b5ff3b2c067e4d
s0kil <dtechtours@gmail.com>
2020-09-21 15:09:30 -0400

    Small Improvments To The palindrome_index Solution

--- Solutions/PalindromeIndex/s0kil/s0kil.rb
+++ Solutions/PalindromeIndex/s0kil/s0kil.rb
@@ -1,12 +1,9 @@
 def palindrome_index(string)
-  is_palindrome = ->(s) { s == s.reverse }
+  is_palindrome = ->(str) { str == str.reverse }
 
   return -1 if string.length <= 1 || is_palindrome.call(string)
 
-  string_without_char =
-    lambda { |s, char_index|
-      s[0...char_index] + s[(char_index + 1)..-1]
-    }
+  string_without_char = ->(str, char_index) { str[0...char_index] + str[(char_index + 1)..-1] }
 
   string.each_char.with_index do |char, char_index|
     last_char_index = string.length - char_index - 1
@@ -14,12 +11,7 @@ def palindrome_index(string)
     # Optimization To Skip Current Iteration If Both Sides Equal
     next if char == last_char
 
-    if is_palindrome.call(string_without_char.call(string, char_index))
-      # Current Char Is The Offender
-      return char_index
-    elsif is_palindrome.call(string_without_char.call(string, last_char_index))
-      # Last Char Is The Offender
-      return last_char_index
-    end
+    return char_index if is_palindrome.call(string_without_char.(string, char_index))
+    return last_char_index if is_palindrome.call(string_without_char.(string, last_char_index))
   end
 end
