51f8a80c1a09d370fe4bd4b6fe20a9f70f7b3369
Daniel Sokil <dtechtours@gmail.com>
2020-12-24 03:29:29 +0000

    Explicity Define Prelude Imports

--- Solutions/Codewars/FindTheFirstNonConsecutiveNumber/s0kil/s0kil.hs
+++ Solutions/Codewars/FindTheFirstNonConsecutiveNumber/s0kil/s0kil.hs
@@ -1,4 +1,5 @@
 import Data.List (nubBy)
+import Prelude (Bool, Int, Maybe (Just, Nothing), abs, (-), (>))
 
 firstNonConsecutive :: [Int] -> Maybe Int
 firstNonConsecutive = firstBy (\a b -> abs (a - b) > 1)
